63027016047158bc032aa66283ec9c5aef19af1e
Aayush-Bisht1 <aayushbisht205@gmail.com>
2025-08-23 23:24:55 +0530

    user context

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -2,16 +2,19 @@
 import { api } from '@/convex/_generated/api';
 import { useUser } from '@clerk/nextjs';
 import { useMutation } from 'convex/react';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { userDetailContext } from '@/context/userDetailContext';
+import { createContext } from 'react';
 
 function Provider({ children }: any) {
     const { user } = useUser();
+    const [userDetail, setUserDetail] = useState<any>(null);
     const createUser = useMutation(api.users.createNewUser);
 
     useEffect(() => {
         user && createNewUser();
-    },[user])
-    
+    }, [user])
+
     const createNewUser = () => {
         if (user) {
             const result = createUser({
@@ -20,11 +23,20 @@ function Provider({ children }: any) {
                 imageUrl: user?.imageUrl || ""
             });
             console.log(result);
+            setUserDetail(result);
         }
     }
     return (
-        <div>{children}</div>
+        <userDetailContext.Provider value={{userDetail,setUserDetail}} >
+            <div>
+                {children}
+            </div>
+        </userDetailContext.Provider>
     )
 }
 
 export default Provider
+
+export const useUserDetailContext = () => {
+    return createContext(userDetailContext);
+}
